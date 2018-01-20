498a937cc34510b25e8b0f4d67bc2df9d8f5cf75
Tung Phan <tungphan@asnet.com.vn>
2018-01-20 09:38:34 +0700

    Add Unit testing

---
+++ src/test/java/tp/weather/domain/WeatherTest.java
@@ -0,0 +1,19 @@
+package tp.weather.domain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class WeatherTest {
+
+	@Test
+	public void createWeather() {
+		assertThat(1).isEqualTo(1);
+	}
+}
