dda8093fb8118cd18595328d5d3ae7d600f94196
Tung Phan <tungphan@asnet.com.vn>
2018-01-20 09:54:29 +0700

    remove jupiter api

--- src/test/java/tp/weather/domain/WeatherTest.java
+++ src/test/java/tp/weather/domain/WeatherTest.java
@@ -1,15 +1,8 @@
 package tp.weather.domain;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class WeatherTest {
 
 	@Test
